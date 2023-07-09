5ed9ad5c557758f24f5d0977db3771963b99f9b6
ctrlouis <lo.bertschy@gmail.com>
2023-07-10 00:48:52 +0200

    migration to oak for cors in progress

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-import { serve } from "https://deno.land/std@0.193.0/http/server.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const PORT = Deno.env.get("PORT") ? Number(Deno.env.get("PORT")) : 1993 ;
 const DOLIBARR_API_BASE = Deno.env.get("DOLIBARR_API_BASE");
 const DOLIBARR_API_KEY = Deno.env.get("DOLIBARR_API_KEY");
 
-const handler = async (request: Request): Promise<Response> => {
-    const baseUrl = new URL(request.url);
+const handler = async (ctx) => {
+    const baseUrl = new URL(ctx.request.url);
     const params = baseUrl.href.replace(baseUrl.origin,'');
     const fetchUrl = `https://${DOLIBARR_API_BASE}${params}`;
     console.log(fetchUrl);
     console.log(DOLIBARR_API_KEY);
-    let res;
     try {
         const jsonResponse  = await fetch(fetchUrl, {
             method: "GET",
@@ -20,14 +20,16 @@ const handler = async (request: Request): Promise<Response> => {
             },
         });
         const jsonData = await jsonResponse.json();
-        res = jsonData;
+        ctx.response = jsonData;
     } catch (error) {
         console.error(error);
-        res = error.message;
+        ctx.response = error.message;
     }
-    
-    return new Response(JSON.stringify(res), { status: 200 });
 };
 
-console.log(`HTTP webserver running. Access it at: http://localhost:${PORT}/`);
-await serve(handler, { port: PORT });
+const app = new Application();
+app.use(oakCors()); // Enable CORS for All Routes
+app.use(handler);
+
+console.info(`Web server listening on port ${PORT}`);
+await app.listen({ port: PORT });
